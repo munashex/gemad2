442cdfb7898807de0404df1ce30d790e2ea93ef5
munashe <munashemugondaa@gmail.com>
2026-08-04 21:21:42 +0200

    fixing bug

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,8 +12,8 @@ import HeroImage from '/images/contact/contact-hero.jpg'
 
 // 👇 YOUR EMAILJS CREDENTIALS (from emailjs.com dashboard)
 const EMAILJS_SERVICE_ID = 'service_2d4mdg5'
-const EMAILJS_TEMPLATE_ID = 'template_gfk1d2n'
-const EMAILJS_PUBLIC_KEY = 'TScleMrx7otXkKfDW'
+const EMAILJS_TEMPLATE_ID = 'template_ri1bgi8'
+const EMAILJS_PUBLIC_KEY = 'TScIeMrx7otXkKfDW'
 
 // Scroll animation variants
 const fadeInUp = {
